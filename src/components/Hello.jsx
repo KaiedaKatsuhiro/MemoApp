ade8af6bb901af4094ce64a3c44b83ad4c9f887b
KAI <73045639+KaiedaKatsuhiro@users.noreply.github.com>
2021-08-13 18:16:08 +0900

    Creaate Hello component

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -1,25 +1,27 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import { bool, string } from 'prop-types';
+import { bool, shape, string } from 'prop-types';
 
 function Hello(props) {
-  const { children, bang } = props;
+  const { children, bang, style } = props;
   return (
     <View>
-      <Text style={styles.text}>
+      <Text style={[styles.text, style]}>
         {`Hello ${children} ${bang ? '!' : ''}`}
       </Text>
     </View>
   );
 }
 
-Hello.prototype = {
+Hello.propTypes = {
   children: string.isRequired,
   bang: bool,
+  style: shape(),
 };
 
 Hello.defaultProps = {
   bang: false,
+  style: null,
 };
 
 const styles = StyleSheet.create({
